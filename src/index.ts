292561cdfa6fc013028472535979c69cd4dcbfd0
zhouyuan <zhouyuan10@staff.weibo.com>
2023-01-24 20:55:38 +0800

    submit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const calc = (a: number, b: number): number => {
+  return a - b
+}
+
+console.log('=====calc', calc(1024, 28))
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+function test(): string {
+  const web: string = 'hello world'
+  // console.log(web)
+  return web
+}
+
+console.log('======test', test())
